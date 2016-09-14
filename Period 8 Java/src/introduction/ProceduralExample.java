35ff96a129ca6b441d62668613aae77b30cd377b
Student 8 <Student 8@10.8.33.150>
2016-09-14 13:56:08 -0400

    Second

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -2,7 +2,8 @@ package introduction;
 
 public class ProceduralExample { 
   public static void main(String[] args){
-      System.out.printIn("Hello World")
+      System.out.println("Hello World");
+      System.out.println("ro6 hates wad3");
       // system = class
       // out = field 
       //printIn = method of that field
