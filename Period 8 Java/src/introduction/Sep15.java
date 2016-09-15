08aedc42e194d84c4aaf56ac24398e43028acba1
Student 8 <Student 8@10.8.33.150>
2016-09-15 13:44:18 -0400

    asd

---
+++ Period 8 Java/src/introduction/Sep15.java
@@ -0,0 +1,8 @@
+package introduction;
+
+public class Sep15 {
+	 public static void main(String[] args){
+		 	String[] names = { "Ivan", "xd", "hehe" };
+		 	 System.out.println("Not" + names[0]);   
+	  }
+}
