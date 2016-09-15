e37e7ffb974d970a8bc8b784ab24e352b8335b9b
L0hL <izhen7813@bths.edu>
2016-09-15 13:52:00 -0400

    hi my name is

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -8,6 +8,7 @@ public class ProceduralExample {
       //printIn = method of that field
     String[] years = {"Freshmen", "Sophmore", "Junior", "Senior"};
     String [] names = { "Josh", "Alvin", "George", "Vincent"};
+    System.out.println("Hi, my name is" + names[0] + "and I am a " + years[0] + ".");
     }
   
 }
