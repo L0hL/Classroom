2ab7f590c3da737f9880e39a217debb0a1f26825
Student 8 <Student 8@10.8.33.150>
2016-09-27 14:03:57 -0400

    asd

--- Period 8 Java/src/ChatBot8/IvanMain.java
+++ Period 8 Java/src/ChatBot8/IvanMain.java
@@ -24,44 +24,75 @@ public class IvanMain {
 			print("Hi"+ user + ". How are you?");
 			response = promptInput();
 		}
-		if(findkeyword()){
+		if(findkeyword(response,"good",0) >= 0){
 			print("That's wonderful. So glad you feel good.");
 		}
-		while (response.indexOf("school")>= 0){
+		else if (response.indexOf("school")>= 0){
 			print("School is great. Tell me about School.");
 			inMainLoop=false;
 			school.talk();
 		}
+		else{
+			print("I don't understand.");
+		}
 	}
-	public static boolean findkeyword(String searchString, String keyword, int StartPsn){
+	// this is a helper method. designed to help larger method because of this helper methods are generally private 
+	//because they are only used by the method they are helping. Also when you do your project, I expect to see helper methods because they make the code more readable.
+	// will return true if no negation words from the methods
+	private static boolean noNegations(String searchString, 
+			int psn){
+		//check to see if no is from psn
+			//check to see if 3 spaces in front. then check to see if no is there
+		if(psn - 3 >= 0 && searchString.substring(psn-3,psn).equals("no ")){
+			return false;
+		}
+		//check for not
+		if(psn - 6>=0 && searchString.substring(psn-6,psn).equals("never")){
+			return false;
+		}
+		if(psn - 4>=0 && searchString.substring(psn-4,psn).equals("not ")){
+			return false;
+		}
+		return true;
+	}
+	public static int findkeyword(String searchString, String keyword, int StartPsn){
+		
 		//delete white space
 		searchString = searchString.trim();
 		searchString = searchString.toLowerCase();
 		keyword = keyword.toLowerCase();
+		System.out.println("the key word is found in at");
 		//find first position of key word
 		int psn = searchString.indexOf(0);
 		//keep searching until context keyword found
+		System.out.println("the key is" + keyword);
+		System.out.println(" the key is found at"+ psn);
+		
 		while(psn>=0){
 			//assume proceeded and followed by space
-			String before = " ";
-			String after = " ";
+			String before = "";
+			String after = "";
 			if(psn>0){
 					before = searchString.substring(psn-1, psn);
 			}
 			if(psn+keyword.length()<searchString.length()){
 				after = searchString.substring(psn+keyword.length(), psn+keyword.length()+1);
 			}
-			if(before.compareTo("a")<0){
-				//psn+1 is one space after current
+			if(before.compareTo("a")<0 && after.compareTo("a")<0 && noNegations(searchString,psn)){
+				//pos+1 is one space after current
+				return 0;
+			}
+			else{
 				psn = searchString.indexOf(keyword,psn+1);
 			}
 		}
+		return psn;
 	}
 
 	public static void DemoStringMethod(){
 
 		createScanner();
-	//	promptInput();
+		promptInput();
 		createField();
 		promptName();
 	}
@@ -77,9 +108,7 @@ public class IvanMain {
 				+ "You can call me Bob. We should become friends.");
 	}
 	public static String  promptInput(){
-		print("Please Type Something.");
 		String userInput = input.nextLine();
-		print("Congratulations You Typed:" + userInput);
 		return userInput;
 	}
 
