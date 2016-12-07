b1f4c5e33a8667847ec9a23bc3dd80fa61ff5f1a
Student 8 <Student 8@10.8.33.150>
2016-12-07 14:04:22 -0500

    asdasdasd

---
+++ Period 8 Java/src/GUIpractice/Screen.java
@@ -0,0 +1,32 @@
+package GUIpractice;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public class Screen {
+
+	private int width;
+	private int height;
+	protected BufferedImage image; // almost like private but accessible to other classes
+	public Screen(int width, int height){
+		this.width = width;
+		this.height = height;
+		initImage();
+	}
+
+	private void initImage() {
+		image = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
+		update();
+	}
+
+	public void update() {
+		// this is where you draw stuff
+		Graphics2D g = image.createGraphics();
+		g.drawString("Hello", 40, 40);
+	}
+	
+	public BufferedImage getImage(){
+		return image;
+	}
+
+}
